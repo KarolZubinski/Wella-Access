ea6da58a9816eca6675991971e8649d3ebbbe337
KarolZubinski <zubinski.k@gmail.com>
2025-06-25 20:38:07 +0200

    added homepage.spec.ts

---
+++ tests/tests/homepage.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
+
+test.only('should not have any automatically detectable WCAG A or AA violations', async ({ page }) => {
+  await page.goto('https://www.wella.com/professional/en-EN');
+
+  const accessibilityScanResults = await new AxeBuilder({ page })
+      .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
+      .analyze();
+
+  expect(accessibilityScanResults.violations).toEqual([]);
+});
