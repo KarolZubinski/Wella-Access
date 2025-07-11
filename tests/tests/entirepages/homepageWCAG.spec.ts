2b14f5f3664f82cadc81ad867e07fb9515a6725f
KarolZubinski <zubinski.k@gmail.com>
2025-07-11 08:56:49 +0200

    added attach to test

--- tests/tests/entirepages/homepageWCAG.spec.ts
+++ tests/tests/entirepages/homepageWCAG.spec.ts
@@ -1,13 +1,20 @@
-import { test, expect } from '@playwright/test';
-import AxeBuilder from '@axe-core/playwright';
+import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
-test('should not have any automatically detectable WCAG A or AA violations', async ({ page }) => {
-  await page.goto('https://www.wella.com/professional/en-EN');
-//   await page.goto('https://bbc.co.uk/');
+test("should not have any automatically detectable WCAG A or AA violations", async ({
+  page,
+}, testInfo) => {
+  await page.goto("https://www.wella.com/professional/en-EN");
+  //   await page.goto('https://bbc.co.uk/');
 
   const accessibilityScanResults = await new AxeBuilder({ page })
-      .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
-      .analyze();
+    .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"])
+    .analyze();
+
+  await testInfo.attach("accessibility-scan-results", {
+    body: JSON.stringify(accessibilityScanResults, null, 2),
+    contentType: "application/json",
+  });
 
   expect(accessibilityScanResults.violations).toEqual([]);
 });
